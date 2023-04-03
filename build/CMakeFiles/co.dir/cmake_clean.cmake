4932506521dbd73add14609822bc9dfa0d8270f2
Cosmo Julis <cosmo.julis@gmail.com>
2023-04-03 06:32:13 +0000

    initial

---
+++ build/CMakeFiles/co.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/co.dir/main.cpp.o"
+  "co"
+  "co.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/co.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
